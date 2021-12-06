9020ea46ee4b9ecd9df3a99ae66bb3582ddbd40f
tlamlert <tanadol_lamlertprasertkul@brown.edu>
2021-12-05 23:06:42 -0500

    add show_data

--- code/show_data.py
+++ code/show_data.py
@@ -1,6 +1,6 @@
 import pickle
 
-def show_data(input_sentence_file, correct_sentence_file, label_file, num_data):
+def show_data(input_sentence_file, correct_sentence_file, input_pos_file, label_file, num_data):
     with open(input_sentence_file, 'rb') as file:
         sentences = pickle.load(file)
         file.close()
@@ -11,19 +11,24 @@ def show_data(input_sentence_file, correct_sentence_file, label_file, num_data):
         file.close()
         print(correction[:num_data])
 
+    with open(input_pos_file, 'rb') as file:
+        input_pos = pickle.load(file)
+        file.close()
+        print(input_pos[:num_data])
+
     with open(label_file, 'rb') as file:
         errors = pickle.load(file)
         file.close()
         print(errors[:num_data])
 
-    for word, err_type in zip(sentences[:num_data], errors[:num_data]):
-        print(f"{word}, {err_type}")
+    for word, pos, err_type in zip(sentences[:num_data], input_pos[:num_data], errors[:num_data]):
+        print(f"{word}, {pos}, {err_type}")
 
 if __name__ == '__main__':
     # file location
     directory = '../processed_dataset/training/npltea16_HSK_TrainingSet/'
     input_sentence_file = directory + 'input_sentences'
-    # input_pos_file = directory + 'input_pos'
+    input_pos_file = directory + 'input_pos'
     correct_sentence_file = directory + 'correct_sentences'
     label_file = directory + 'errors'
 
@@ -31,4 +36,4 @@ if __name__ == '__main__':
     # end =
     num_data = 100
 
-    show_data(input_sentence_file, correct_sentence_file, label_file, num_data)
+    show_data(input_sentence_file, correct_sentence_file, input_pos_file, label_file, num_data)
